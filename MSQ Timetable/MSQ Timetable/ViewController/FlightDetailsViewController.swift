540af1675d04b2ab375fe54a45045929ec072d02
Aliaksandr Niakhai <sashanehai@gmail.com>
2020-01-16 07:27:35 +0300

    Important fixes of code review feedback

---
+++ MSQ Timetable/MSQ Timetable/ViewController/FlightDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlightDetailsViewController.swift
+//  MSQ Timetable
+//
+//  Created by Александр Нехай on 1/14/20.
+//  Copyright © 2020 AlexanderNehai. All rights reserved.
+//
+
+import UIKit
+
+class FlightDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
